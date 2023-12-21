14c4acb2e000d6602ab2b62eb62cc1b56838bfd0
ykp <ykp9711@gmail.com>
2023-12-21 17:10:56 +0900

    Spring boot 기본설정123

---
+++ SNS/src/test/java/com/ykp/SNS/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykp.SNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
